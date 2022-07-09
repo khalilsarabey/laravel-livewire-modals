083d6e394db71e6f8ceff640aee710c293810f76
Khalil Sarabey <hello@khalilsarabey.com>
2022-07-10 02:51:58 +0300

    Update modals.js

--- resources/js/modals.js
+++ resources/js/modals.js
@@ -1,23 +1,13 @@
-import {Modal} from 'bootstrap';
-
 let modalsElement = document.getElementById('laravel-livewire-modals');
 
-modalsElement.addEventListener('hidden.bs.modal', () => {
-    Livewire.emit('resetModal');
+$(modalsElement).on('hidden.bs.modal', () => {
+  Livewire.emit('resetModal');
 });
 
 Livewire.on('showBootstrapModal', () => {
-    let modal = Modal.getInstance(modalsElement);
-
-    if (!modal) {
-        modal = new Modal(modalsElement);
-    }
-
-    modal.show();
+  $(modalsElement).modal('show');
 });
 
 Livewire.on('hideModal', () => {
-    let modal = Modal.getInstance(modalsElement);
-
-    modal.hide();
+  $(modalsElement).modal('hide');
 });
